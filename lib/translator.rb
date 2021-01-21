18cb45431dcaaf8b1baf9290d9fc543b6d960d51
Marissa Nolan <ritz1484@cs.com>
2021-01-21 22:49:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,12 +14,12 @@ def load_library(file_path)
   binding.pry
 end
 
-def get_japanese_emoticon(emoticon)
+def get_japanese_emoticon(lib, emoticon)
   # takes in the parameter of an emoticon and outputs the value from the key, value pair 
   #of the outer key,value pair from the load_library method above
   lib = load_library(yaml)
     lib.map do |emoticon|
-      
+      emoticon = 
     
 end
 
